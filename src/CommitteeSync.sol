34325cd6efc23bd1b1454bad789d359fb43203ad
Deddy Dayag <daviddayag@gmail.com>
2024-09-26 15:33:42 +0300

    no message

--- src/CommitteeSync.sol
+++ src/CommitteeSync.sol
@@ -13,6 +13,7 @@ import "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
  *      potential misuse. All committee operations are protected by role-based access control.
  */
 contract CommitteeSync is AccessControl, ReentrancyGuard {
+
     // Roles
     bytes32 public constant COMMITTEE_ROLE = keccak256("COMMITTEE_ROLE");
 
@@ -37,6 +38,7 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
     // Events
     event CommitteeUpdated(address[] newCommittee);
     event ProposalSubmitted(bytes32 proposalHash);
+    
     event ProposalApproved(bytes32 proposalHash);
 
     /**
@@ -44,11 +46,17 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
      * @param initialCommittee The initial set of committee members.
      */
     constructor(address[] memory initialCommittee) {
+
         _setCommittee(initialCommittee);
+
         // Grant COMMITTEE_ROLE to each initial committee member
+
         for (uint256 i = 0; i < initialCommittee.length; i++) {
+
             _grantRole(COMMITTEE_ROLE, initialCommittee[i]);
+
         }
+
     }
 
     /**
@@ -68,23 +76,26 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
      */
     function proposeOrApprove(address[] memory newCommittee, uint256 approvalDeadline)
     external
-    onlyCommittee
-    checkApprovalDeadline(approvalDeadline)
-    {
+    onlyCommittee checkApprovalDeadline(approvalDeadline) {
+
         require(newCommittee.length > 0 && newCommittee.length <= MAX_COMMITTEE_SIZE, "Invalid committee size");
 
         bytes32 proposalHash = keccak256(abi.encode(newCommittee));
 
         // Check if proposal exists
         if (proposals[proposalHash].proposalDeadline == 0) {
+
             // Proposal doesn't exist, create a new one
             Proposal storage proposal = proposals[proposalHash];
+
             proposal.newCommittee = newCommittee;
             proposal.proposalDeadline = block.timestamp + PROPOSAL_DEADLINE;
 
-            proposalHashes.push(proposalHash);
             // Track the proposal hash
+            proposalHashes.push(proposalHash);
+
             emit ProposalSubmitted(proposalHash);
+
         }
 
         // Check if sender has already approved
@@ -97,18 +108,24 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
 
         // Check if enough approvals to update the committee
         if (existingProposal.approvals >= currentCommittee.length / 2) {
+
             _setCommittee(existingProposal.newCommittee);
+
             emit CommitteeUpdated(existingProposal.newCommittee);
-            delete proposals[proposalHash];
+
             // Clean up proposal
-            _removeProposalHash(proposalHash);
+            delete proposals[proposalHash];
+
             // Remove from proposalHashes list
+            _removeProposalHash(proposalHash);
+
         }
 
         emit ProposalApproved(proposalHash);
 
         // Call maintenance to clean up expired data
         maintenance();
+
     }
 
     /**
@@ -116,17 +133,26 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
      * @dev This function should be called periodically to remove expired proposals.
      */
     function maintenance() public nonReentrant {
+
         // Iterate only over the existing proposals in proposalHashes
+
         for (uint256 i = 0; i < proposalHashes.length; i++) {
+
             bytes32 proposalHash = proposalHashes[i];
+
             Proposal storage proposal = proposals[proposalHash];
+
             if (proposal.proposalDeadline != 0 && block.timestamp > proposal.proposalDeadline) {
-                delete proposals[proposalHash];
+
                 // Delete the expired proposal
-                _removeProposalHash(proposalHash);
+                delete proposals[proposalHash];
+
                 // Remove from proposalHashes list
-                i--;
+                _removeProposalHash(proposalHash);
+
                 // Adjust index after deletion
+                i--;
+
             }
         }
     }
@@ -136,15 +162,19 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
      * @param newCommittee The new committee members.
      */
     function _setCommittee(address[] memory newCommittee) internal {
+
         // Revoke COMMITTEE_ROLE from previous committee members
         for (uint256 i = 0; i < currentCommittee.length; i++) {
             _revokeRole(COMMITTEE_ROLE, currentCommittee[i]);
         }
+
         currentCommittee = newCommittee;
+
         // Grant COMMITTEE_ROLE to new committee members
         for (uint256 j = 0; j < newCommittee.length; j++) {
             _grantRole(COMMITTEE_ROLE, newCommittee[j]);
         }
+
     }
 
     /**
@@ -152,13 +182,19 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
      * @param proposalHash The hash of the proposal to be removed.
      */
     function _removeProposalHash(bytes32 proposalHash) internal {
+
         // Find the index of the proposalHash in the array and remove it
+
         for (uint256 i = 0; i < proposalHashes.length; i++) {
+
             if (proposalHashes[i] == proposalHash) {
+
                 proposalHashes[i] = proposalHashes[proposalHashes.length - 1];
                 proposalHashes.pop();
                 break;
+
             }
+
         }
     }
 
@@ -170,17 +206,23 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
      * @return approvals The number of approvals the proposal has received
      */
     function getProposal(bytes32 proposalHash) public view returns (address[] memory newCommittee, uint256 proposalDeadline, uint256 approvals) {
+
         Proposal storage proposal = proposals[proposalHash];
+
         require(proposal.proposalDeadline != 0, "Proposal does not exist");
+
         return (proposal.newCommittee, proposal.proposalDeadline, proposal.approvals);
+
     }
 
     /**
      * @dev Modifier to ensure that the sender is a committee member.
      */
     modifier onlyCommittee() {
+
         require(hasRole(COMMITTEE_ROLE, msg.sender), "Not a committee member");
         _;
+
     }
 
     /**
@@ -188,8 +230,10 @@ contract CommitteeSync is AccessControl, ReentrancyGuard {
      * @param approvalDeadline The deadline for the approval to be valid.
      */
     modifier checkApprovalDeadline(uint256 approvalDeadline) {
+
         require(block.timestamp <= approvalDeadline, "Approval deadline has passed");
         _;
+
     }
 
     /**
