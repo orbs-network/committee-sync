11be34e5de985304dfc214700753a20ef45d0222
DanielZlotin <danielzlotin@users.noreply.github.com>
2026-01-14 13:15:57 +0200

    deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,8 +7,11 @@ import "src/CommitteeSync.sol";
 
 contract Deploy is Script {
     function run() public returns (address committeeSync) {
+        address owner = vm.envAddress("OWNER");
+        console.logBytes32(hashInitCode(type(CommitteeSync).creationCode, abi.encode(owner)));
+
         vm.broadcast();
-        CommitteeSync deployed = new CommitteeSync{salt: 0}(vm.envAddress("OWNER"));
+        CommitteeSync deployed = new CommitteeSync{salt: 0}(owner);
         committeeSync = address(deployed);
     }
 }
