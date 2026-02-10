6eaa214c140de196529b3f7f5b0f87abaca03ae8
DanielZlotin <danielzlotin@users.noreply.github.com>
2026-02-10 13:59:12 +0200

    deployments

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,8 +11,21 @@ contract Deploy is Script {
         bytes32 salt = vm.envOr("SALT", bytes32(0x75e3be5a0037b707320866345cfb398f5401fc5736077dcdadfa9e4c6737210b));
         console.logBytes32(hashInitCode(type(CommitteeSync).creationCode, abi.encode(owner)));
 
+        committeeSync = _getExistingDeployment();
+        if (committeeSync != address(0)) {
+            console.log("already deployed");
+            return committeeSync;
+        }
+
         vm.broadcast();
         CommitteeSync deployed = new CommitteeSync{salt: salt}(owner);
         committeeSync = address(deployed);
+        console.log("deployed", committeeSync);
+    }
+
+    function _getExistingDeployment() internal view returns (address existing) {
+        try vm.getDeployment("CommitteeSync", uint64(block.chainid)) returns (address deployedAddress) {
+            existing = deployedAddress;
+        } catch {}
     }
 }
